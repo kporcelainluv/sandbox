45a9604dfdc358b93474d192ad4a5220be3b4d74
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-09 15:38:44 +0300

    Initialize project using Create React App

---
+++ src/stories/2-Card.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "../App";
+
+export const AppComponent = () => <App title={"my world"} />;
+export default {
+  title: "App",
+  component: AppComponent
+};
