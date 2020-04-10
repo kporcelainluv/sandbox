6fa87ebd8f93ee1d36b720322867abedece45466
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-10 22:00:28 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { Card } from "./components/Card";
+
 export const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="container">
+      <Card
+        description="Read redux article"
+        date={"11 april"}
+        time={"13:00"}
+        hashtags={"#interview"}
+      />
+    </div>
+  );
 };
