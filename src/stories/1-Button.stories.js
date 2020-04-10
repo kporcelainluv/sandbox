13a6216510fa9b272ce9fe7cf7b0159064a06c38
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-10 22:00:24 +0300

    add storybook components

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+
+import { Button } from "../components/Button";
+
+export const Small = () => <Button text={"Delete"} classname="button--small" />;
+
+export const Medium = () => (
+  <Button text={"Delete"} classname="button--medium" />
+);
+
+export const Large = () => <Button text={"Delete"} classname="button--large" />;
+
+export const Xlarge = () => (
+  <Button text={"Delete"} classname="button--xlarge" />
+);
+
+export const Outline = () => (
+  <Button text={"Delete"} classname="button--outline" />
+);
+
+export const DarkBlue = () => (
+  <Button text={"Delete"} classname="button--blue" />
+);
+
+export const Green = () => <Button text={"Delete"} classname="button--green" />;
+
+export const Yellow = () => (
+  <Button text={"Delete"} classname="button--yellow" />
+);
+
+export const Orange = () => (
+  <Button text={"Delete"} classname="button--orange" />
+);
 
 export default {
-  title: 'Button',
-  component: Button,
+  title: "Button"
 };
-
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
-
-export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
--- src/stories/2-Card.stories.js
+++ src/stories/2-Card.stories.js
@@ -1,8 +1,17 @@
 import React from "react";
-import App from "../App";
+import "../index.css";
 
-export const AppComponent = () => <App title={"my world"} />;
+import { Card } from "../components/Card";
+export const Default = () => (
+  <Card
+    description="Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even
+others for their massive size."
+    date={"23 september"}
+    time={"21:11"}
+    hashtags={"#helloWorld"}
+  />
+);
+
 export default {
-  title: "App",
-  component: AppComponent
+  title: "Card"
 };
